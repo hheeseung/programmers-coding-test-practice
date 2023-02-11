672777432f79b1c4f0668d9190c392810568e226
hheeseung <ssigu69@gmail.com>
2023-02-11 16:01:33 +0900

    commit: Day 2

---
+++ 007.js
@@ -0,0 +1,7 @@
+// https://school.programmers.co.kr/learn/courses/30/lessons/120809
+
+function solution(numbers) {
+  let answer = [];
+  answer = numbers.map((item) => item * 2);
+  return answer;
+}
